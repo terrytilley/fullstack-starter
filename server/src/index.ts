ab0e3776c384f7d5bae1434284832250e530edf5
Terry Tilley <terry@terrytilley.com>
2020-08-21 23:58:18 +0100

    setup apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,33 @@
-import { MikroORM } from '@mikro-orm/core'
-import { __prod__ } from './constants'
-import { Post } from './entities/Post'
-import ormConfig from './mikro-orm.config'
+import express from 'express';
+import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+
+import ormConfig from './mikro-orm.config';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
-  const orm = await MikroORM.init(ormConfig)
+  const orm = await MikroORM.init(ormConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: 'My Post' });
-  // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {})
-  console.log('🚀', { posts })
-}
+  const port = process.env.PORT || 4000;
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log('🚀', `Server started on localhost:${port}`);
+  });
+};
 
 main().catch((err) => {
   console.error(err);
