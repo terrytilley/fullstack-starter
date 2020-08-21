498e5234a45833c2ce42cbc3e8b3f13acda226d9
Terry Tilley <terry@terrytilley.com>
2020-08-22 00:47:19 +0100

    mikro-orm crud

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,16 +1,22 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { ObjectType, Field, Int } from 'type-graphql';
 
 @Entity()
+@ObjectType()
 export class Post {
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
+  @Field(() => Date)
   @Property({ type: 'date' })
   createdAt = new Date();
 
+  @Field(() => Date)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express from 'express';
 import { MikroORM } from '@mikro-orm/core';
 import { ApolloServer } from 'apollo-server-express';
@@ -5,8 +6,8 @@ import { buildSchema } from 'type-graphql';
 
 import ormConfig from './mikro-orm.config';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(ormConfig);
@@ -17,9 +18,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
