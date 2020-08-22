16666cdfef529f38f7d127e2daf029630488ef67
Terry Tilley <terry@terrytilley.com>
2020-08-22 02:42:26 +0100

    login resolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import argon2 from 'argon2';
-import { Resolver, Ctx, Mutation, InputType, Field, Arg } from 'type-graphql';
+import { Resolver, Ctx, Mutation, InputType, Field, Arg, Query, ObjectType } from 'type-graphql';
 
 import { User } from '../entities/User';
 import { MyContext } from '../types';
@@ -13,16 +13,106 @@ export class CredentialsInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+export class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
-  async register(@Arg('input') input: CredentialsInput, @Ctx() { em }: MyContext): Promise<User> {
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
+  }
+
+  @Mutation(() => UserResponse)
+  async register(@Arg('input') input: CredentialsInput, @Ctx() { em }: MyContext): Promise<UserResponse> {
+    if (input.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'length must be greater than 2',
+          },
+        ],
+      };
+    }
+
+    if (input.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'length must be greater than 3',
+          },
+        ],
+      };
+    }
+
     const hashedPassword = await argon2.hash(input.password);
     const user = em.create(User, {
       username: input.username,
       password: hashedPassword,
     });
-    await em.persistAndFlush(user);
-    return user;
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      // duplicate username error
+      if (err.code === '23505') {
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'username has already been taken',
+            },
+          ],
+        };
+      }
+    }
+
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(@Arg('input') input: CredentialsInput, @Ctx() { em }: MyContext): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: input.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: "username doesn't exist",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, input.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'incorrect password',
+          },
+        ],
+      };
+    }
+
+    return { user };
   }
 }
