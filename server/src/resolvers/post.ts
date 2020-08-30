c5bf6851d0476956e18d54350f2b5fca2fffa07a
Terry Tilley <terry@terrytilley.com>
2020-08-30 22:58:19 +0100

    fetch-post-creator

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -48,19 +48,45 @@ export class PostResolver {
   ): Promise<PaginatedPosts> {
     const maxLimit = Math.min(50, limit);
     const maxLimitPlusOne = maxLimit + 1;
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder(`p`)
-      .orderBy(`"createdAt"`, `DESC`)
-      .take(maxLimitPlusOne);
+    const replacements: any[] = [maxLimitPlusOne];
 
     if (cursor) {
-      qb.where(`"createdAt" < :cursor`, {
-        cursor: new Date(parseInt(cursor)),
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    const posts = await qb.getMany();
+    const posts = await getConnection().query(
+      `
+      select p.*,
+      json_build_object(
+        'id', u.id,
+        'username', u.username,
+        'email', u.email,
+        'createdAt', u."createdAt",
+        'updatedAt', u."updatedAt"
+        ) creator
+      from post p
+      inner join public.user u on u.id = p."creatorId"
+      ${cursor ? `where p."createdAt" < $2` : ''}
+      order by p."createdAt" DESC
+      limit $1
+    `,
+      replacements
+    );
+
+    // const qb = getConnection()
+    //   .getRepository(Post)
+    //   .createQueryBuilder(`p`)
+    //   .innerJoinAndSelect(`p.creator`, `u`, `u.id = p."creatorId"`)
+    //   .orderBy(`p."createdAt"`, `DESC`)
+    //   .take(maxLimitPlusOne);
+
+    // if (cursor) {
+    //   qb.where(`p."createdAt" < :cursor`, {
+    //     cursor: new Date(parseInt(cursor)),
+    //   });
+    // }
+
+    // const posts = await qb.getMany();
 
     return {
       posts: posts.slice(0, maxLimit),
